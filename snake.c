e773b15cd7960c77dff3a1ffd95e28e22f6aa20b
xerxes1920 <67414049+xerxes1920@users.noreply.github.com>
2021-12-13 09:47:48 -0600

    Update snake.c

--- snake.c
+++ snake.c
@@ -5,18 +5,12 @@
 
 const uint_8 snakeBitmaps[] =
 {
-	0x20, //   #    
-	0x70, //  ###   
-	0xF0, // ####   
-	0x20, //   #    
-	0x00, //        
-	0x20, //   #    
-	0x00, //        
-	0x20, //   #    
-	0x00, //        
-	0x20, //   #    
+	0xF0, // ####
+	0xF0, // ####
+	0xF0, // ####
+	0xF0, // ####
 };
 
 // Bitmap sizes for snake
-const uint_8 snakeWidthPixels = 7;
-const uint_8 snakeHeightPixels = 10;
+const uint_8 snakeWidthPixels = 4;
+const uint_8 snakeHeightPixels = 4;
