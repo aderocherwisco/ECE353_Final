5f5cba7ee817e5218d283303567bd40743023ae6
xerxes1920 <67414049+xerxes1920@users.noreply.github.com>
2021-12-13 08:56:31 -0600

    Add files via upload

---
+++ snake.c
@@ -0,0 +1,21 @@
+// 
+//  Image data for snake
+// 
+
+const uint_8 snakeBitmaps =
+{
+	0x20, //   #    
+	0x70, //  ###   
+	0xF0, // ####   
+	0x20, //   #    
+	0x00, //        
+	0x20, //   #    
+	0x00, //        
+	0x20, //   #    
+	0x00, //        
+	0x20, //   #    
+};
+
+// Bitmap sizes for snake
+const uint_8 snakeWidthPixels = 7;
+const uint_8 snakeHeightPixels = 10;
---
+++ snake.h
@@ -0,0 +1,4 @@
+// Bitmap info for snake
+extern const uint_8 snakeBitmaps;
+extern const uint_8 snakeWidthPixels;
+extern const uint_8 snakeHeightPixels;
